fd7db725f827e2098f6dc19f44a2d65c3f20d496
Yeshey Zhennue <02240372.cst@rub.edu.bt>
2026-05-11 00:56:01 +0530

    updated

--- be/src/main.ts
+++ be/src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   );
 
   const port = Number(process.env.PORT ?? 5000);
-  await app.listen(port);
+  await app.listen(port, "0.0.0.0");
   console.log(`Backend running on port ${port}`);
 }
 
